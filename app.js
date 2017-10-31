c6c10fcfb0512cd4b0f8dfbeec086d37ca72c5a7
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2017-10-30 23:24:37 -0500

    Setup routing

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 
 const app = express();
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = {
+      hello: 'Hi, I am the response'
+    };
+    res.json(response);
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API!!!');
