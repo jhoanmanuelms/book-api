1fce40628bd3ed3f86e54a883c266818cb8fe3f3
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2017-11-02 00:06:17 -0500

    - Setup connection to MongoDB - Create end point to query the books stored in the database

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const db = mongoose.connect('mongodb://localhost/bookAPI', { useMongoClient: true });
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = {
-      hello: 'Hi, I am the response'
-    };
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
   });
 
 app.use('/api', bookRouter);
